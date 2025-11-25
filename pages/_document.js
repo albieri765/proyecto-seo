027a16f855b434aff28874cb99622044c55caa3b
Albieri Vilcape Gutierrez <albieri.vilcape@tecsup.edu.pe>
2025-11-25 15:43:07 -0500

    Tarea_Lab14A_VILCAPE2

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+// pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="es">
+    <Html>
       <Head>
-        {/* Google Search Console Verification */}
-        <meta 
-          name="google-site-verification" 
-          content="NAW6QVTiQ2YWyOhbAxn9bYxNxZb580zvdqivq4YOkVk" 
-        />
+        <meta name="google-site-verification" content="lGocxbN6TXUQXr1XHdFHzZeKf7O4UZvYAvppHHAPy5c" />
       </Head>
       <body>
         <Main />
